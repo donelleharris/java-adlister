d8a136bef5fb4c2ffb6d183ace8fad4a56561753
Donelle Harris <donelle.harris@gmail.com>
2020-11-08 11:13:40 -0600

    Complete JDBC exercises

---
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -0,0 +1,16 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.Config;
+
+public class DaoFactory {
+    public static Ads adsDao;
+
+    private static Config config = new Config();
+
+    public static Ads getAdsDao() {
+        if (adsDao == null) {
+            adsDao = new MySQLAdsDao(config);
+        }
+        return adsDao;
+    }
+}
